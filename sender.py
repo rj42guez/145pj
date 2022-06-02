318afd1f30ebcca3fcb7c5834f75a389a605f1c6
Raphael Justin C. Portuguez <rj42guez@gmail.com>
2022-06-02 19:04:22 +0800

    hello

--- sender.py
+++ sender.py
@@ -2,14 +2,10 @@ import binascii
 import socket
 import struct
 import hashlib
-import scapy 
+#import scapy
 import sys
 
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-## printing the hostname and ip_address
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
+
 
 def compute_checksum(packet):
     return hashlib.md5(packet.encode('utf-8')).hexdigest()
@@ -27,32 +23,46 @@ path = ''
 
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
+
+
 args = sys.argv
 
 for i in range(len(args)):
-	if args[i] == '-f':
-		path = ''
-	if args[i] == '-a':
-		ipR = args[i+1]
-		print(ipR, '\n') 
-	if args[i] == '-s':
-		portR = int(args[i+1])
-		print(portR, '\n') 
-	if args[i] == '-c':
-		portS = int(args[i+1])
-		print(portS, '\n')
-	if args[i] == '-i':
-		iD = args[i+1] 
-		print(iD, '\n')
+        if args[i] == '-f':
+                path = ''
+        if args[i] == '-a':
+                ipR = args[i+1]
+                print(ipR, '\n')
+        if args[i] == '-s':
+                portR = int(args[i+1])
+                print(portR, '\n')
+        if args[i] == '-c':
+                portS = int(args[i+1])
+                print(portS, '\n')
+        if args[i] == '-i':
+                iD = args[i+1]
+                print(iD, '\n')
 
-M = encodeMessage(iM)
+M = encodeMessage('ID'+iD)
+print(M)
 udpSocket.sendto(M, (ipR, portR))
 
 #udpSocket.bind(('', portR))
 
-#while True:
-#	data, addr = udpSocket.recvfrom(1024)
-#	if len(data) > 0:
-#		print(addr,'\n')
-#		print(data.decode())
-#		break
+
+udpSocket2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udpSocket2.bind(('',portS))
+#tcpSocket.listen(5)
+
+
+
+while True:
+
+	data = udpSocket2.recvfrom(1024)
+
+	if len(data) > 0:
+
+		print(data.decode())
+
+		break
