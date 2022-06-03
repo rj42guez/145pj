21f0268509e08d95fd44a471f7a628022cccaec4
Raphael Justin C. Portuguez <rj42guez@gmail.com>
2022-06-03 15:25:00 +0800

    acks

--- sender.py
+++ sender.py
@@ -20,49 +20,68 @@ portS = 6734
 portR = 0
 ipR = '10.0.7.141'
 path = ''
+transID = '0'
 
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
-
 
 args = sys.argv
 
 for i in range(len(args)):
         if args[i] == '-f':
-                path = ''
+                path = args[i+1]
         if args[i] == '-a':
                 ipR = args[i+1]
-                print(ipR, '\n')
         if args[i] == '-s':
                 portR = int(args[i+1])
-                print(portR, '\n')
         if args[i] == '-c':
                 portS = int(args[i+1])
-                print(portS, '\n')
         if args[i] == '-i':
                 iD = args[i+1]
-                print(iD, '\n')
 
-M = encodeMessage('ID'+iD)
+M = encodeMessage('ID6d93e931')
+udpSocket.bind(('',portS))
 print(M)
+
 udpSocket.sendto(M, (ipR, portR))
 
-#udpSocket.bind(('', portR))
+data, addr = udpSocket.recvfrom(4096)
 
+if len(data) > 0:
+	print(addr)
+	transID = data.decode()
 
-udpSocket2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-udpSocket2.bind(('',portS))
-#tcpSocket.listen(5)
+print(transID)
 
+f = open(path, 'r')
+pyld = f.read()
 
+seq = 0
 
-while True:
+for i in range(0, len(pyld), 10):
 
-	data = udpSocket2.recvfrom(1024)
+	if len(pyld) - i >= 10:
+		pyld_sub = pyld[i:i+10].encode()
+		z = 0
+
+	elif len(pyld) - i < 10:
+		pyld_sub = pyld[i:].encode()
+		z = 1
+
+	w = iD
+	x = '{0:07d}'.format(seq)
+	y = transID
+
+	packet = 'ID{}SN{}TXN{}LAST{}{}'.format(w, x, y, z, pyld_sub)
+
+	print(packet)
+
+	M = encodeMessage(packet)
+	udpSocket.sendto(M, (ipR, portR))
+
+	data, addr = udpSocket.recvfrom(4096)
 
 	if len(data) > 0:
-
+		print(addr)
 		print(data.decode())
-
-		break
+		
+	seq += 1
